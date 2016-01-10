50753e822b3600ef1ff432c8784429b2024bd2c7
Y292450104 <292450104@qq.com>
2016-01-10 22:15:10 +0800

    [Y292450104]update managedbean

---
+++ src/com/my/framework/init/ManagedBeanWrapper.java
@@ -0,0 +1,62 @@
+package com.my.framework.init;
+
+/**
+ * 
+ * @author Administrator
+ * 
+ * 用于map中的value， key为bean的name
+ */
+public class ManagedBeanWrapper {
+	private String className;
+	@SuppressWarnings("unused")
+	private String beanName;
+	private Class<?> clazz;
+	private static Object singletonBean;
+	@SuppressWarnings("unused")
+	private String type;
+	
+	public ManagedBeanWrapper(String className) {
+		this(className, false);
+	}
+	
+	public ManagedBeanWrapper(String className, boolean isSingleton) {
+		try {
+			clazz = Thread.currentThread().getContextClassLoader().loadClass(className);
+			singletonBean = clazz.newInstance();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public String getClassName() {
+		return className;
+	}
+	
+	public Object getSingletonBean() {
+		if (null == singletonBean) {
+			throw new RuntimeException(className + " is not a singleton bean!");
+		}
+		return singletonBean;
+	}
+	
+	public Object getBean() {
+		try {
+			return clazz.newInstance();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new RuntimeException("", e);
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new RuntimeException("", e);
+		}
+	}
+}
